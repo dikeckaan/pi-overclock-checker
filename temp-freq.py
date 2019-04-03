d9e518f43d7a4e9a46cac8e0e57fa4b54363203e
KAAN DIKEC <21171681+dikeckaan@users.noreply.github.com>
2019-04-03 01:45:07 +0100

    First bug fixed

--- temp-freq.py
+++ temp-freq.py
@@ -1,16 +1,14 @@
 import os
 import time
 
-temp = os.popen("vcgencmd measure_temp").readline()
-mhz = os.popen("vcgencmd measure_clock arm").readline()
-mhz =mhz[14:]
-mhz =mhz[::-1]
-mhz =mhz[7:]
-mhz =mhz[::-1]
 while True:
-        print (temp)
-        print mhz
-        print ("")
-        time.sleep(1)
-
-
+	temp = os.popen("vcgencmd measure_temp").readline()
+	mhz = os.popen("vcgencmd measure_clock arm").readline()
+	mhz =mhz[14:]
+	mhz =mhz[::-1]
+	mhz =mhz[7:]
+	mhz =mhz[::-1]
+	print (temp)
+	print mhz
+	print ("")
+	time.sleep(1)
